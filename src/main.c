a05cc229d06218633a041af22349e7b8aff9f498
Hubert <cnbjyangjian@gmail.com>
2022-01-28 03:26:19 +0800

    OLED

--- src/main.c
+++ src/main.c
@@ -1,4 +1,62 @@
-﻿#include "sys.h"
+﻿// #include "sys.h"
+// #include "delay.h"
+// #include "usart.h"
+// #include "led.h"
+// #include "key.h"
+// /************************************************
+//  ALIENTEK 探索者STM32F407开发板 实验4
+//  串口实验-HAL库函数版
+//  技术支持：www.openedv.com
+//  淘宝店铺：http://eboard.taobao.com 
+//  关注微信公众平台微信号："正点原子"，免费获取STM32资料。
+//  广州市星翼电子科技有限公司  
+//  作者：正点原子 @ALIENTEK
+// ************************************************/
+
+// int main(void)
+// {
+//     u8 len;
+//     u16 times = 0;
+
+//     HAL_Init();                     //初始化HAL库
+//                    //初始化延时函数
+//         Stm32_Clock_Init(168, 25, 2, 4); //设置时钟,84
+//     delay_init(84);                  //初始化延时函数
+//     uart_init(115200);              //初始化USART
+//     LED_Init();                     //初始化LED
+//     KEY_Init();                     //初始化按键
+
+//     while (1)
+//     {
+
+//         if (USART_RX_STA & 0x8000)
+//         {
+//             len = USART_RX_STA & 0x3fff; //得到此次接收到的数据长度
+//             printf("\r\n您发送的消息为:\r\n");
+//             HAL_UART_Transmit(&UART1_Handler, (uint8_t *)USART_RX_BUF, len, 1000); //发送接收到的数据
+//             while (__HAL_UART_GET_FLAG(&UART1_Handler, UART_FLAG_TC) != SET)
+//                 ;               //等待发送结束
+//             printf("\r\n\r\n"); //插入换行
+//             USART_RX_STA = 0;
+//         }
+//         else
+//         {
+//             times++;
+//             if (times % 5000 == 0)
+//             {
+//                 printf("\r\nALIENTEK 探索者STM32F407开发板 串口实验\r\n");
+//                 printf("正点原子@ALIENTEK\r\n\r\n\r\n");
+//             }
+//             if (times % 200 == 0)
+//                 printf("请输入数据,以回车键结束\r\n");
+//             if (times % 30 == 0)
+//                 SYS_LED = !SYS_LED; //闪烁LED,提示系统正在运行.
+//             delay_ms(10);
+//         }
+//     }
+// }
+
+#include "sys.h"
 #include "delay.h"
 #include "usart.h"
 #include "led.h"
@@ -53,7 +111,7 @@ int main(void)
     LED_Init();                      //初始化LED
     KEY_Init();                      //初始化LED
     uart_init(115200);              //初始化串口
-    usmart_dev.init(84); 		    //初始化USMART	
+    usmart_dev.init(84); 		    //初始化USMART
     // shell_init();
     // shell_register("ssd1306", ssd1306);
     res = ssd1306_basic_init(SSD1306_INTERFACE_IIC, SSD1306_ADDR_SA0_0);
@@ -158,7 +216,7 @@ void gui_task(void *pvParameters)
 
     while (1)
     {
-       
+
         vTaskDelay(1000);
     }
 }
